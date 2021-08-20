732d12c4e67d8fe6163107f2964b3073fc47c814
kowsikgelli <jaikrishnagelli@gmail.com>
2021-08-19 17:05:51 -0700

    Requirement 1:- Fizz Buzz for 1 to 100

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,2 +1,13 @@
 public class FizzBuzz {
+    private int number;
+    public FizzBuzz(int number) {
+        this.number = number;
+    }
+
+    public String findFizzBuzz() {
+        if(this.number%3 == 0 && this.number%5 == 0) return "FizzBuzz";
+        if(this.number%3 == 0) return "Fizz";
+        if(this.number%5 == 0) return "Buzz";
+        return "";
+    }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,2 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+    @Test
+    void shouldReturnFizzBuzzWhenInputIsFifteen(){
+        FizzBuzz fizzBuzz = new FizzBuzz(15);
+        assertEquals("FizzBuzz",fizzBuzz.findFizzBuzz());
+    }
+
+    @Test
+    void shouldReturnFizzWhenInputIsEighteen(){
+        FizzBuzz fizzBuzz = new FizzBuzz(18);
+        assertEquals("Fizz",fizzBuzz.findFizzBuzz());
+    }
+
+    @Test
+    void shouldReturnBuzzWhenInputIsTwentyFive(){
+        FizzBuzz fizzBuzz = new FizzBuzz(25);
+        assertEquals("Buzz",fizzBuzz.findFizzBuzz());
+    }
+
+    @Test
+    void shouldReturnEmptyStringWhenInputIsTwentyThree(){
+        FizzBuzz fizzBuzz = new FizzBuzz(23);
+        assertEquals("",fizzBuzz.findFizzBuzz());
+    }
 }
